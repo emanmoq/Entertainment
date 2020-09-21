6186c70063016b7ae76f61e20b845bc5be66d933
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-09-21 03:48:49 +0300

    dd

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,33 @@ jQuery(function ($) {
 
 
 
+    $(window).scroll(function () {
+      
+        scrollTop = $(window).scrollTop();
+
+        if (scrollTop > $('header').height()) {
+            $("#counter").css("padding-top", "150px");
+            $('header').addClass('scrollNav');
+
+
+        }
+
+        else {
+            $('header').removeClass('scrollNav');
+        }
+        if (($(window).scrollTop() + $(window).height()) > $(document).height()) {
+
+            $(".up").stop(true).animate({
+                opacity: 1
+            }, 100);
+
+        } else {
+
+            $(".up").stop(true).animate({
+                opacity: 0
+            }, 250);
+        }
+    
+    });
 });
 
