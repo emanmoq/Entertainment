7a0c2ddcb13c6a70f526ec9a7248b9f76a5f30a6
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-09-21 03:56:22 +0300

    ss

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,26 +9,37 @@ jQuery(function ($) {
         if (scrollTop > $('header').height()) {
             $("#counter").css("padding-top", "150px");
             $('header').addClass('scrollNav');
-
-
         }
 
         else {
             $('header').removeClass('scrollNav');
+        }    
+    });
+    function onScroll(event){
+        var scrollPosition = $(document).scrollTop();
+      $('.nav-link[href^="#"').each(function () {
+        console.log($(this).attr('href')); 
+        var refElement = $($(this).attr("href"));
+      //   console.log($(this).attr("href")); //log
+        if (refElement.length&&refElement.position().top-100 <= scrollPosition) {
+        $('.nav-link').removeClass("active");
+        $(this).addClass("active");
         }
-        if (($(window).scrollTop() + $(window).height()) > $(document).height()) {
-
-            $(".up").stop(true).animate({
-                opacity: 1
-            }, 100);
-
-        } else {
-
-            $(".up").stop(true).animate({
-                opacity: 0
-            }, 250);
+        else if( $(document).scrollTop() <= 100){
+          $('.nav-link').removeClass("active");
+          $('.nav-link.home').addClass("active");
         }
+        else{
+        $(this).removeClass("active"); 
+      
+        }
+      });
+    }
+    $(document).on("scroll", onScroll);
+    $('.navbar-collapse .nav-link').on('click', function () {
+      $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
     
+      return false;
     });
 });
 
